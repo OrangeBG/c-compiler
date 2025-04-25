235cb71ee6ad2700f769d9986c8efc2b55057fdb
OrangeBG <dvargas531@yahoo.com>
2025-04-25 12:39:57 -0500

    Starting Intermediate representation

---
+++ include/intermediate_rep.h
@@ -0,0 +1,36 @@
+#ifndef INTERMEDIATE_REP
+#define INTERMEDIATE_REP
+
+#include "parser.h"
+
+typedef struct IRNode IRNode;
+
+typedef enum {
+  IR_PROGRAM,
+  IR_FUNCTION,
+  IR_INSTRUCTION_RET,
+  IR_INSTRUCTION_UNARY,
+  IR_VALUE,
+  IR_UNARY_OP
+} IRNodeType;
+
+typedef enum {
+  IR_UNARY_COMPLEMENT,
+  IR_UNARY_NEGATE
+} IRUnaryOpType;
+
+typedef struct IRNode {
+ IRNodeType type;
+ union {
+  struct IRProgram { struct IRNode *function;  } program;
+  struct IRFunction { char *identifier; int instruction_count; int instruction_capacity; IRNode *instructions; } function;
+  struct IRInstructionReturn { struct IRNode *value; } instruction_ret;
+  struct IRInstructionUnary { IRUnaryOpType op_type; IRNode *source; IRNode *destination; } unary;
+  struct IRValueConstant { int value; } value_constant;
+  struct IRValueVar { char *identifier; } value_var;
+ } data; 
+} IRNode;
+
+IRNode* generate_intermediate_rep(AstNode *ast_node);
+
+#endif
---
+++ src/intermediate_rep.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include "../include/intermediate_rep.h"
+
+IRNode* generate_intermediate_rep(AstNode *ast_node) {
+  IRNode *ir_nodes;
+
+  return ir_nodes;
+}
