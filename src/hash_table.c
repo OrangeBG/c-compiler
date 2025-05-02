bd63758edacc07153eca37bc75b9b62e897e9451
OrangeBG <dvargas531@yahoo.com>
2025-05-02 08:59:20 -0500

    Removing hash table print statements

--- src/hash_table.c
+++ src/hash_table.c
@@ -13,7 +13,6 @@ void            hash_table_expand(HashTable *table, int new_capacity);
 HashTableEntry* hash_table_get_with_entries(HashTableEntry *entries, int capacity, char *key); 
 
 void hash_table_init(HashTable *table) {
-  printf("Initializing hash table\n");
   table->capacity = 0;
   table->count = 0;
   table->entries = NULL;
@@ -26,8 +25,6 @@ void hash_table_delete_entry(HashTable *table, char *key) {
     return;
   }
 
-  printf("deleting hash entry\n");
-
   entry->key = NULL;
   entry->value.type = HASH_TOMBSTONE;
 }
@@ -36,7 +33,6 @@ void hash_table_add_entry(HashTable *table, HashTableEntry *entry) {
   uint32_t hash = hash_key(entry->key);
 
   if (table->count + 1 > table->capacity * HASH_TABLE_MAX_LOAD) {
-    printf("Expanding hash table..");
     int new_capacity = table->capacity < HASH_TABLE_INITIAL_CAPACITY ? HASH_TABLE_INITIAL_CAPACITY : table->capacity * 2;
     hash_table_expand(table, new_capacity);    
   }
@@ -47,8 +43,6 @@ void hash_table_add_entry(HashTable *table, HashTableEntry *entry) {
 }
 
 void hash_table_expand(HashTable *table, int new_capacity) {
-  printf("Expanding hash table from %d to %d\n", table->capacity, new_capacity);
-
   HashTableEntry *new_entries = malloc(sizeof(HashTableEntry) * new_capacity);
 
   for (int i = 0; i < new_capacity; i++) {
@@ -58,12 +52,9 @@ void hash_table_expand(HashTable *table, int new_capacity) {
   table->count = 0;
 
   for (int i = 0; i < table->capacity; i++) {
-    printf("Processing idx %d\n", i);
-
     HashTableEntry old_entry = table->entries[i];
 
     if (old_entry.value.type == HASH_TOMBSTONE) {
-      printf("Tombstone found, moving on\n");
       continue;
     }
 
@@ -74,8 +65,6 @@ void hash_table_expand(HashTable *table, int new_capacity) {
     
     table->count++;
   }
-
-  printf("Done expanding\n");
 
   table->capacity = new_capacity;
   table->entries = new_entries;  
@@ -104,6 +93,7 @@ uint32_t hash_key(char *key) {
   return hash;
 }
 
+//Using Linear probing
 HashTableEntry* hash_table_get_with_entries(HashTableEntry *entries, int capacity, char *key) {
   uint32_t hash = hash_key(key);
   int index = hash % capacity;
@@ -116,11 +106,9 @@ HashTableEntry* hash_table_get_with_entries(HashTableEntry *entries, int capacit
     }
     
     if (entry->key != NULL && strcmp(entry->key, key) == 0) {
-      printf("Hash entry found\n");
       return entry;
     }
 
-    printf("Incrementing index search: %d\n", index);
     index = (index + 1) % capacity;
   }
 }
