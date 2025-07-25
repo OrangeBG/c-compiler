5f8b6a83a2d7feade95ce07e66150e5f8c045c61
danny <dvargas531@yahoo.com>
2025-07-25 14:14:55 -0500

    Moving pc's

--- src/sa_variable_resolution.c
+++ src/sa_variable_resolution.c
@@ -14,8 +14,8 @@ enum DeclarationType {
 };
 
 static void variable_resolve_node(AstNode *node, Stack *declaration_stack); 
-static void resolve_file_scope_variable_declaration(char *identifier, enum DeclarationType declaration_type, Stack *declaration_stack);  
-static void resolve_local_scope_variable_declaration(char *identifier, enum DeclarationType declaration_type, Stack *declaration_stack);   
+static void resolve_file_scope_variable_declaration(char *identifier, enum DeclarationType declaration_type, HashTable *declarations);  
+static void resolve_local_scope_variable_declaration(char *identifier, enum DeclarationType declaration_type, HashTable *declarations);   
 
 typedef struct {
   char *name;
@@ -25,8 +25,8 @@ typedef struct {
 } Declaration;
 
 void sa_variable_resolution(AstNode *ast_nodes) {
-  Stack *declaration_stack;
-  stack_init(declaration_stack, VARIABLE_RESOLUTION_STACK_SIZE);
+  Stack declaration_stack;
+  stack_init(&declaration_stack, VARIABLE_RESOLUTION_STACK_SIZE);
 
   HashTable symbols_table;
   hash_table_init(&symbols_table);
@@ -35,11 +35,11 @@ void sa_variable_resolution(AstNode *ast_nodes) {
   file_scope_stack.type = STACK_STRUCT;
   file_scope_stack.data.structure = &symbols_table;
 
-  stack_push(declaration_stack, file_scope_stack);
+  stack_push(&declaration_stack, file_scope_stack);
 
   for (int i = 0; i < ast_nodes->data.program.declaration_count; i++) {
     AstNode *declaration_node = ast_nodes->data.program.declaration_ptrs->node_pointers[i];
-    variable_resolve_node(declaration_node, declaration_stack);
+    variable_resolve_node(declaration_node, &declaration_stack);
   }
 }
 
@@ -50,27 +50,30 @@ static void variable_resolve_node(AstNode *node, Stack *declaration_stack) {
   }
   
   switch (node->type) {
-    case AST_VARIABLE_DECLARATION:
+    case AST_VARIABLE_DECLARATION: {
+      StackValue *declaration_top_stack = stack_top(declaration_stack);
+      HashTable *declaration_table = declaration_top_stack->data.structure;
+
       if (declaration_stack->count == 1) {
-        resolve_file_scope_variable_declaration(node->data.variable_declaration.name, DECLARATION_TYPE_VARIABLE, declaration_stack);
+        resolve_file_scope_variable_declaration(node->data.variable_declaration.name, DECLARATION_TYPE_VARIABLE, declaration_table);
       } else {
-        resolve_local_scope_variable_declaration(node->data.variable_declaration.name, DECLARATION_TYPE_VARIABLE, declaration_stack);   
+        resolve_local_scope_variable_declaration(node->data.variable_declaration.name, DECLARATION_TYPE_VARIABLE, declaration_table);   
       }
 
       if (node->data.variable_declaration.has_expression == true) {
         variable_resolve_node(node->data.variable_declaration.init_expression, declaration_stack);
       }      
       break;
+    }
     default:
       fprintf(stderr, "ERROR - SA Variable Resolution: AST type '%d' not supported", node->type);
       exit(1);
       break;
-
   }
 } 
 
-static void resolve_file_scope_variable_declaration(char *identifier, enum DeclarationType declaration_type, Stack *declaration_stack) {  
-  HashTableEntry *table_entry = hash_table_get_entry(declaration_stack->stack->data.structure, identifier);
+static void resolve_file_scope_variable_declaration(char *identifier, enum DeclarationType declaration_type, HashTable *declaration_table) {  
+  HashTableEntry *table_entry = hash_table_get_entry(declaration_table, identifier);
 
   Declaration *file_scope_declaration = malloc(sizeof(Declaration));
   file_scope_declaration->declaration_type = declaration_type;
@@ -82,6 +85,6 @@ static void resolve_file_scope_variable_declaration(char *identifier, enum Decla
   table_entry->value->structure = file_scope_declaration;
 }
 
-static void resolve_local_scope_variable_declaration(char *identifier, enum DeclarationType declaration_type, Stack *declaration_stack) {  
+static void resolve_local_scope_variable_declaration(char *identifier, enum DeclarationType declaration_type, HashTable *declarations) {  
 
 }
