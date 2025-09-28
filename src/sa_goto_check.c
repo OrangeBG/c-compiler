1fb7570b8d99898c8b72e89992f07d4808562e34
OrangeBG <dvargas531@yahoo.com>
2025-09-27 23:26:30 -0500

    Initial check in

---
+++ src/sa_goto_check.c
@@ -0,0 +1,11 @@
+#include "../include/sa_goto_check.h"
+
+static void check_ast_node(AstNode *ast_node);
+
+void sa_goto_check(AstNode *ast_nodes) {
+  check_ast_node(ast_nodes);
+}
+
+void check_ast_node(AstNode *ast_node) {
+
+}
