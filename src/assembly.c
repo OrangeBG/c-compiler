41d29968b5df3f8178010a9f50498e102c94a7d6
OrangeBG <dvargas531@yahoo.com>
2025-10-04 21:18:21 -0500

    Added emit_instruction_cvtsi2sd() in assembly.c

--- src/assembly.c
+++ src/assembly.c
@@ -41,6 +41,7 @@ static void         emit_instruction_function_call(AsmNode *asm_function, IRNode
 static void         emit_instruction_sign_extend(AsmNode *asm_function, IRNode *ir_sign_extend_instruction, Arena *asm_arena, AsmNodePointers *top_level_declarations, DeclarationSymbolTable *declaration_symbol_table); 
 static void         emit_instruction_zero_extend(AsmNode *asm_function, IRNode *ir_zero_extend_instruction, Arena *asm_arena, AsmNodePointers *top_level_declarations, DeclarationSymbolTable *declaration_symbol_table);
 static void         emit_instruction_truncate(AsmNode *asm_function, IRNode *ir_truncate_instruction, Arena *asm_arena, AsmNodePointers *top_level_declarations, DeclarationSymbolTable *declaration_symbol_table); 
+static void         emit_instruction_cvtsi2sd(AsmNode *asm_function, IRNode *ir_int_to_double_instruction, Arena *asm_arena, AsmNodePointers *top_level_declarations, DeclarationSymbolTable *declaration_symbol_table); 
 static void         add_instruction_to_function(AsmNode *function, AsmNode *instruction); 
 static void         add_to_node_pointer(AsmNode *asm_node, AsmNodePointers *asm_node_pointer);
 static void         init_node_pointer(AsmNodePointers *asm_node_pointer);
@@ -836,6 +837,9 @@ static void emit_function(IRNode *ir_function, AsmNode *asm_function, Arena *asm
         case IR_INSTRUCTION_TRUNCATE:
           emit_instruction_truncate(asm_function, current_ir_node, asm_arena, top_level_declarations, declaration_symbol_table);
           break;
+        case IR_INSTRUCTION_INT_TO_DOUBLE:
+          emit_instruction_cvtsi2sd(asm_function, current_ir_node, asm_arena, top_level_declarations, declaration_symbol_table);
+          break;
       default:
         fprintf(stderr, "ERROR - Assembler: Could not resolve instruction type in asm_function\n");
         exit(1);
@@ -1541,6 +1545,20 @@ static void emit_instruction_truncate(AsmNode *asm_function, IRNode *ir_truncate
 static void add_instruction_to_function(AsmNode *function, AsmNode *instruction) {
   add_to_node_pointer(instruction, function->data.function.instruction_pointers);
   function->data.function.instruction_count++;
+}
+
+static void emit_instruction_cvtsi2sd(AsmNode *asm_function, IRNode *ir_int_to_double_instruction, Arena *asm_arena, AsmNodePointers *top_level_declarations, DeclarationSymbolTable *declaration_symbol_table) {
+  AsmNode *source_node = create_operand(ir_int_to_double_instruction->data.instruction_int_to_double.source, asm_arena, top_level_declarations, declaration_symbol_table);
+  AsmNode *destination_node = create_operand(ir_int_to_double_instruction->data.instruction_int_to_double.destination, asm_arena, top_level_declarations, declaration_symbol_table);
+  AsmType source_type = convert_ir_value_to_asm_type(ir_int_to_double_instruction->data.instruction_int_to_double.source, declaration_symbol_table);
+
+  AsmNode *cvtsi2sd_instruction = arena_alloc(asm_arena);
+  cvtsi2sd_instruction->type = ASM_INSTRUCTION_CVTSI2SD;
+  cvtsi2sd_instruction->data.instruction_cvtsi2sd.source_operand = source_node;
+  cvtsi2sd_instruction->data.instruction_cvtsi2sd.destination_operand = destination_node;
+  cvtsi2sd_instruction->data.instruction_cvtsi2sd.source_assembly_type = source_type;
+
+  add_instruction_to_function(asm_function, cvtsi2sd_instruction);
 }
 
 static AsmNode* create_operand(IRNode *ir_operand, Arena *asm_arena, AsmNodePointers *top_level_pointers, DeclarationSymbolTable *declaration_symbol_table) {
