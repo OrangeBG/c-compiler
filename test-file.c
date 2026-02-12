ed5d6cb4adfe868ff4137eeaaddf1dd3839523e9
OrangeBG <dvargas531@yahoo.com>
2026-02-11 22:39:11 -0600

    Initial check in

--- test-file.c
+++ test-file.c
@@ -1,9 +1,13 @@
 int main(void) {
-    //static int nested[1][2] = { { 0, 0 } };
+    int nested[3][2] = { { 0, 0 }, { 1, 2 }, {3, 5} };
+    int i = 0;
+    int j = 1;
+    int test = nested[i][j];     
+    
+    // int nested[3] = {3, 5, 6};
+    // int i = 2;
+    
+    // int test = nested[i];     
 
-    int array[3] = { 1, 2, 3 };
-    int *ptr = array - 1;
-
-    int help = *ptr;
     return 0;
 }
